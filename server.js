7abccec2580c6132816a09d06734f6a0290e2a36
Dmitry Pimenov <dmitryspimenov@gmail.com>
2017-03-18 14:21:53 -0700

    lets see

--- server.js
+++ server.js
@@ -10,42 +10,9 @@ app.get('/crawl/:href(*)', function (req, res) {
     visited = []
     console.log("parms", startUrl);
     horizon.push(format(startUrl))
-    explore(horizon, visited, phoneNumbers, request);
-
-   /* horizon.push(format(startUrl)); //testing
-
-    while (horizon.length > 0) {
-        currentUrl = horizon.pop();
-        console.log("current url: ", currentUrl)
-        if (!visited.includes(currentUrl)) {
-            //issue request
-            request(currentUrl, function (error, response, body) {
-                //parse for phone numbers and urls 
-                //console.log(error);
-                //console.log(response);
-                if (!error && response && response.statusCode == 200) {
-                    //parse for urls
-                    urlMatches = body.match(/\w+\.(com|org|net)\/*\w*(\"|\'){1}/g)//body.match(/\w+\.\w{3,}\"/g);
-                    urlMatchesCleaned = [];
-                    for (let i = 0; i < urlMatches.length; i++) {
-                        let curr = urlMatches[i];
-                        urlMatchesCleaned.push(curr.substring(0, curr.length - 1)); //remove last char
-                    }
-                    console.log(urlMatchesCleaned);
-                    for (let i = 0; i < urlMatchesCleaned.length; i++) {
-                        horizon.push(format(urlMatchesCleaned));
-                    }
-                    console.log(horizon);
-                    //parse for phone numbers                
-                    phoneMatches = body.match(/\(?\d{3}\)?-{1}\d{3}-{1}\d{4}/g)
-
-                }
-                visited.push(currentUrl)
-            });
-        }
-    } */
+    explore(horizon, visited, phoneNumbers, request, res);
 })
-
+   
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
@@ -54,8 +21,7 @@ function format(url) {
     return "https://" + url;
 }
 
-
-function explore(horizon, visited, phoneNums, request) {
+function explore(horizon, visited, phoneNums, request, response) {
     if (horizon.length > 0){
         currentUrl = horizon.pop();
         console.log("current url: ", currentUrl);
@@ -84,14 +50,13 @@ function explore(horizon, visited, phoneNums, request) {
                     phoneNums.push(phoneMatches);
                     visited.push(currentUrl)
                 } 
-                explore(horizon, visited, phoneNums, request);
+                explore(horizon, visited, phoneNums, request, res);
             });
         } else {
-            explore(horizon, visited, phoneNums, request);
+            explore(horizon, visited, phoneNums, request, res);
         }
     } else {
-        console.log("all done");
-        return phoneNums;
+        res.write(phoneNums)
     }
 }
 
